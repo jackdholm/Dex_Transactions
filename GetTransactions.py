d9f7456f281daaf5fbd3e33a21f01f06e7154a56
jackdholm <jackdholm1@gmail.com>
2020-11-19 18:04:53 -0600

    - GetTransactions now prints to csv file - Updated gitignore

--- GetTransactions.py
+++ GetTransactions.py
@@ -1,12 +1,24 @@
 import api_key_config
 import TransactionList
 import sys
+import csv
+
+rowNames = ["Timestamp", "Asset Traded", "Cost", "Transaction Fee", "Amount Traded", "Asset Received", "Amount Received"]
 
 if len(sys.argv) < 2:
     print ("Address argument required")
     exit()
+
 address = str(sys.argv[1])
-print(address)
+if len(sys.argv) >= 3 and len(str(sys.argv[2])) > 0:
+    filename = str(sys.argv[2])
+else:
+    filename = "Transaction_List.csv"
+
 txList = TransactionList.GetTransactions(address, api_key_config.API_KEY)
-for i in txList:
-    print(i)
+
+with open(filename, "w", newline="") as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow(rowNames)
+    for row in txList:
+        writer.writerow(row)
